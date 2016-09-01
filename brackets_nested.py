849bc95ecf4e477b57ee1b1b95adce3e40fc8f1b
puszolek <kochanska.paula@gmail.com>
2016-08-31 19:06:47 -0700

    brackets

---
+++ brackets_nested.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+d
+
+ef solution(S):
+    
+    if len(S) == 0:
+        return 1
+        
+    begining = ['[', '{', '(']
+    diction = {
+        ']' : '[', 
+        '}' : '{', 
+        ')' : '(',
+        }
+    
+    a = []
+    for i in S:
+        if i in begining:
+            a.append(i)
+        else:
+            if len(a) == 0:
+                return 0
+            elif diction[i] != a.pop():
+                return 0
+                
+    if len(a) == 0:
+        return 1
+    else:
+        return 0
+        
+    pass
